f476e26e14efeca0e8949f708e87f63cdcfc2a1f
Sasha Khamkov <sanusart@gmail.com>
2014-10-13 23:56:30 +0300

    CHANGE: domain in config

--- js/main.js
+++ js/main.js
@@ -15,3 +15,24 @@ $(function () {
     });
 
 });
+
+var _gas = _gas || [];
+_gas.push(['_setAccount', 'UA-17599340-1']);
+_gas.push(['_setDomainName', '.sanusart.com']);
+_gas.push(['_trackPageview']);
+_gas.push(['_gasTrackForms']);
+_gas.push(['_gasTrackOutboundLinks']);
+_gas.push(['_gasTrackMaxScroll']);
+_gas.push(['_gasTrackDownloads']);
+_gas.push(['_gasTrackYoutube', {force: true}]);
+_gas.push(['_gasTrackVimeo', {force: true}]);
+_gas.push(['_gasTrackMailto']);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = '//cdnjs.cloudflare.com/ajax/libs/gas/1.10.1/gas.min.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
