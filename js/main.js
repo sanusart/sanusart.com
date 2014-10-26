bfcf3ddf53b7ffaf014936ed0d69e3a6bbc15e61
Sasha Khamkov <sanusart@gmail.com>
2014-10-26 10:04:34 +0200

    FIX: inline analytics

--- js/main.js
+++ js/main.js
@@ -39,32 +39,14 @@ $(function () {
             }, 'fast');
         });
 
+        $('.page--home h1').html('<i class="fa fa-chevron-down"></i>');
+
     } else {
         $('nav > ul').show();
     }
 
+
 });
-
-var _gas = _gas || [];
-_gas.push(['_setAccount', 'UA-17599340-1']);
-_gas.push(['_setDomainName', '.sanusart.com']);
-_gas.push(['_trackPageview']);
-_gas.push(['_gasTrackForms']);
-_gas.push(['_gasTrackOutboundLinks']);
-_gas.push(['_gasTrackMaxScroll']);
-_gas.push(['_gasTrackDownloads']);
-_gas.push(['_gasTrackYoutube', {force: true}]);
-_gas.push(['_gasTrackVimeo', {force: true}]);
-_gas.push(['_gasTrackMailto']);
-
-(function () {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = '//cdnjs.cloudflare.com/ajax/libs/gas/1.10.1/gas.min.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
 
 function isMobile() {
     return (Modernizr.mq('only all and (max-width: 600px)')) ? true : false;
